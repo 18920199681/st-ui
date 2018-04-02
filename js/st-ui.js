4a8497d7e3516d600e761c8daeb34091f545b307
guodong <547800128@qq.com>
2018-04-02 17:50:01 +0800

    navbar_pc

--- js/st-ui.js
+++ js/st-ui.js
@@ -7,35 +7,43 @@ window.onresize = function () {
 *导航条
 */
 function st_navbar(option) {
-    var st_navbar = '<div class="st_navbar">\
-                        <div class="st_navbar_box clearFloat">\
-                            <div class="st_navbar_left clearFlaot">\
-                                <div class="st_navbar_logo">\
-                                    <img src="'+ option.logo + '" alt="logo">\
-                                </div>\
-                                <div class="st_navbar_company_name">' + option.company_name + '</div>\
+
+    var logo = '';
+    var company_name = '';
+    if (option.logo && typeof (option) == "object") {
+        logo = option.logo;
+    }
+    if (option.company_name && typeof (option) == "object") {
+        company_name = option.company_name;
+    }
+
+    var st_navbar = '<div class="st_navbar_box clearFloat">\
+                        <div class="st_navbar_left clearFlaot">\
+                            <div class="st_navbar_logo">\
+                                <img src="'+ logo + '">\
                             </div>\
-                            <div class="st_navbar_list">\
-                                <ul class="clearFlaot"></ul >\
-                                <div class="st_navbar_list_btn">\
-                                        <div>\
-                                            <span class="st_bavbar_list_btn_line"></span>\
-                                            <span class="st_bavbar_list_btn_line"></span>\
-                                            <span class="st_bavbar_list_btn_line"></span>\
-                                        </div>\
-                                </div>\
-                            </div >\
-                        </div >\
-                    </div >\
-                    <div class="st_body_box">\
-                        <div class="st_navbar_list_phone">\
-                            <ul>&nbsp;</ul>\
+                            <div class="st_navbar_company_name">' + company_name + '</div>\
                         </div>\
-                    </div>';
+                        <div class="st_navbar_list">\
+                            <ul class="clearFlaot"></ul >\
+                            <div class="st_navbar_list_btn">\
+                                <div>\
+                                    <span class="st_bavbar_list_btn_line"></span>\
+                                    <span class="st_bavbar_list_btn_line"></span>\
+                                    <span class="st_bavbar_list_btn_line"></span>\
+                                </div>\
+                            </div>\
+                        </div >\
+                    </div >';
 
-    $('body').html(st_navbar);
+    $('.st_navbar').html(st_navbar);
 
-    var item = option.navbar_menu;
+    var item = [];
+    if (option.navbar_menu && option.navbar_menu instanceof Array) {
+        item = option.navbar_menu;
+    } else {
+        console.error("navbar_menu 应为数组");
+    }
     var menu_list = '';
     for (var i = 0; i < item.length; i++) {
         menu_list += '<a href="javascript:;">\
@@ -44,20 +52,6 @@ function st_navbar(option) {
     }
     $('.st_navbar_list ul').html(menu_list);
 
-    // 菜单（移动端）
-    $('.st_navbar_list_phone ul').html($('.st_navbar_list ul').html());
-    // $('.st_navbar_list_btn').on('click', function (e) {
-    //     let et_navbar_height = $('.st_navbar')[0].offsetHeight;
-    //     let et_body_top = $('.st_body').offset().top;
-    //     let et_navbar_list_height = $('.st_navbar_list_phone')[0].offsetHeight;
-    //     let et_body = document.querySelector(".st_body");
-    //     if (parseInt(et_body_top) == et_navbar_height) {
-    //         startMove(et_body, { top: et_navbar_list_height });
-    //     } else {
-    //         startMove(et_body, { top: 0 });
-    //     }
-    // });
 }
-
 
 
