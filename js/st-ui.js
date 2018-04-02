2166484826ee0d7bad2e770d4d65f369b4a1dee1
guodong <547800128@qq.com>
2018-04-02 19:42:57 +0800

    navbar-ok

--- js/st-ui.js
+++ js/st-ui.js
@@ -51,7 +51,38 @@ function st_navbar(option) {
                         </a>'
     }
     $('.st_navbar_list ul').html(menu_list);
+    $('.st_navbar_menu_phone ul').html(menu_list);
 
+
+    /**
+     * 菜单（移动端）
+     */
+    // 让移动端导航菜单处于收起状态
+    $('body').find('.st_navbar_menu_phone').css('top', -($('.st_navbar_menu_phone')[0].offsetHeight));
+    $('body').find('.st_navbar_list_btn').on('click', function () {
+
+        // 导航条高度
+        let st_navbar_height = $('.st_navbar')[0].offsetHeight;
+        let old_height = $('.st_navbar')[0].offsetHeight;
+
+        // 导航条挡板高度
+        let st_navbar_menu_height_box = document.querySelector('.st_navbar_menu_height');
+        let st_navbar_menu_height = $('.st_navbar_menu_height')[0].offsetHeight;
+
+        // 菜单高度
+        let st_navbar_menu_phone = $('.st_navbar_menu_phone')[0].offsetHeight;
+
+        // 菜单top
+        let st_navbar_menu_phone_top = document.querySelector('.st_navbar_menu_phone');
+
+        if (st_navbar_height == st_navbar_menu_height) {
+            startMove(st_navbar_menu_height_box, { 'height': st_navbar_menu_phone });
+            startMove(st_navbar_menu_phone_top, { 'top': 0 });
+        } else {
+            startMove(st_navbar_menu_height_box, { 'height': old_height });
+            startMove(st_navbar_menu_phone_top, { 'top': -st_navbar_menu_phone });
+        }
+    })
 }
 
 
